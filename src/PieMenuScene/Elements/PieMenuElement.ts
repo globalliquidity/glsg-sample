6db005e6673912cc460a14bb32d6cc25d103aa2a
James Andrew <45604413+james-global-liquidity@users.noreply.github.com>
2019-12-05 11:01:57 -0800

    Adding button model
    Added model loader to webpack.dev.js
    Added pushButton.glb

--- src/PieMenuScene/Elements/PieMenuElement.ts
+++ src/PieMenuScene/Elements/PieMenuElement.ts
@@ -1,9 +1,11 @@
 import * as bjs from 'babylonjs';
+import * as bjsgui from 'babylonjs-gui';
 import { Scene, SceneElement } from '../../glsg';
 import { PieMenuItemElement } from './PieMenuItelmElement';
 
 export class PieMenuElement extends SceneElement
 {
+    itemModel : bjs.Mesh;
     menuItems : Array<PieMenuItemElement> = new Array<PieMenuItemElement>();
 
     constructor(name: string,
@@ -26,6 +28,18 @@ export class PieMenuElement extends SceneElement
 
     protected create()
     {
+        var manager = new bjsgui.GUI3DManager(this.scene.bjsScene);
+        var panel = new bjsgui.Container3D();
+        //panel.margin = 0.75;
+    
+        manager.addControl(panel);
+        panel.linkToTransformNode(this);
+        panel.position.z = -1.5;
+
+        bjs.SceneLoader.ImportMesh("", "../Assets/models/", "pushButton.glb", this.scene.bjsScene, function (newMeshes) {
+            this.itemModel = newMeshes[0];
+        });
+
         for( var i = 0; i < this.itemCount; i++)
         {
             let item :PieMenuItemElement = new PieMenuItemElement("item",
